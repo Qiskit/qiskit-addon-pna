9b95de2b12e3bab461801da5462443835aa1aded
Caleb Johnson <caleb.johnson@ibm.com>
2025-11-09 12:49:05 -0600

    Fix print statements to be robust in a notebook

--- qiskit_addon_pna/pna.py
+++ qiskit_addon_pna/pna.py
@@ -285,11 +285,8 @@ def generate_noise_mitigating_observable(
             # If nothing to do, sleep before checking again
             time.sleep(0.001)
 
-    print(
-        f"Finished! {num_generators} / {num_generators} generators propagated.",
-        end="\r",
-        flush=True,
-    )
+    msg = f"\rFinished! {num_generators} / {num_generators} generators propagated."
+    print(f"\r{msg:<70}", end="", flush=True)
     observable *= global_scale_factor
 
     return observable
