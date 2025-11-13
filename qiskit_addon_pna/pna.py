ed97a4dee11e203bb64d5478261daefdef8a9fa1
Roy Elkabetz <Roy.Elkabetz@ibm.com>
2025-11-13 09:29:58 -0500

    address review

--- qiskit_addon_pna/pna.py
+++ qiskit_addon_pna/pna.py
@@ -108,7 +108,7 @@ def generate_noise_mitigating_observable(
             that more than one error in the batch occurs when the circuit is run. This should usually not be set higher than
             ``max(1, num_processes // 2)``.
         inject_noise_before: If ``True``, the Pauli Lindblad noise instruction will be inserted before its
-         corresponding 2q-gate layer. Otherwise, it will be inserted after it, defaults to ``True``.
+            corresponding 2q-gate layer. Otherwise, it will be inserted after it, defaults to ``True``.
 
     Returns:
         The noise-mitigating observable
